c78583887710c84c82f66bc8a405d239116f535c
Isaddora Freitas <isaddorafreitasar@gmail.com>
2021-08-02 14:45:39 -0300

    add routes + adjust configs

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import routes from './routes/index';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World!'});
-})
+app.get('/', (req, res) => res.json({ message: 'Hello World!' }));
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
-})
+});
