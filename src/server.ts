7ec5a6e09f1364a5c007b2a8724b5db7dea7dff6
Isaddora Freitas <isaddorafreitasar@gmail.com>
2021-08-04 14:39:14 -0300

    add package to get async errors in routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
+import 'express-async-errors';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database'
 
@@ -11,6 +13,20 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.status_code).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
